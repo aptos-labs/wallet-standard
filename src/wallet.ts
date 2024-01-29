9dbecff3c5d2406e8ff19fe5778a9a016563569c
maayan <maayansavir@gmail.com>
2024-01-29 13:21:26 -0500

    add wallet id prop

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,14 +1,20 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-export type { Wallet } from '@wallet-standard/core';
+import { Wallet } from '@wallet-standard/core'
+
+export type { Wallet } from '@wallet-standard/core'
 
 export interface AptosWallet extends Wallet {
+  /**
+   * Website URL of the Wallet
+   */
+  readonly url: string
 
-	/**
- 		* Website URL of the Wallet
- 		*/
-	readonly url: string;
-
+  /**
+   * Unique identifier of the Wallet.
+   *
+   * If not provided, the wallet name will be used as the identifier.
+   */
+  readonly id?: string
 }
-
