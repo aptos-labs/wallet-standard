3549b7a6b72638b9f9d1cc23169858379e3bec6c
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2024-09-06 13:30:52 -0700

    Suppor transaction input and payload input for `signTransaction` (#13)
    * Added `Unsupported` error
    
    * Supporting payload generation input for `signTransaction` and bumped minor

--- src/features/aptosSignTransaction.ts
+++ src/features/aptosSignTransaction.ts
@@ -1,30 +1,82 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-import { AnyRawTransaction, AccountAuthenticator } from '@aptos-labs/ts-sdk'
+import {
+  AccountAddress,
+  AccountAuthenticator,
+  AnyRawTransaction,
+  InputGenerateTransactionPayloadData,
+  Network,
+  PublicKey,
+  TransactionPayload
+} from '@aptos-labs/ts-sdk'
 import { UserResponse } from '../misc'
 
-/** Version of the feature. */
-export type AptosSignTransactionVersion = '1.0.0'
+// region Feature definition
+
 /** Name of the feature. */
 export const AptosSignTransactionNamespace = 'aptos:signTransaction'
-/**
- * A Wallet Standard feature for signing a Aptos transaction, and returning the
- * account authenticator.
- */
-export type AptosSignTransactionFeature = {
-  /** Namespace for the feature. */
+
+export type AptosSignTransactionFeatureV1_0 = {
   [AptosSignTransactionNamespace]: {
-    /** Version of the feature API. */
-    version: AptosSignTransactionVersion
+    version: '1.0.0',
     signTransaction: AptosSignTransactionMethod
   }
 }
+
+export type AptosSignTransactionFeatureV1_1 = {
+  [AptosSignTransactionNamespace]: {
+    version: '1.1'
+    signTransaction: AptosSignTransactionMethod & AptosSignTransactionMethodV1_1
+  }
+}
+
+/**
+ * A Wallet Standard feature for signing an Aptos transaction, and returning the
+ * account authenticator.
+ */
+export type AptosSignTransactionFeature =
+  AptosSignTransactionFeatureV1_0 | AptosSignTransactionFeatureV1_1;
+
+// endregion
+
+// region V1.0
+
+export type AptosSignTransactionOutput = AccountAuthenticator;
 
 export type AptosSignTransactionMethod = (
   transaction: AnyRawTransaction,
   asFeePayer?: boolean
 ) => Promise<UserResponse<AptosSignTransactionOutput>>
 
-/** Output of signing transactions. */
-export type AptosSignTransactionOutput = AccountAuthenticator
+// endregion
+
+// region V1.1
+
+export interface AccountInput {
+  address: AccountAddress;
+  publicKey?: PublicKey;
+}
+
+export interface AptosSignTransactionInputV1_1 {
+  expirationSecondsFromNow?: number; // defaults to 30 seconds (depends on wallet)
+  expirationTimestamp?: number;
+  feePayer?: AccountInput; // defaults to no fee payer
+  gasUnitPrice?: number; // defaults to estimated gas unit price
+  maxGasAmount?: number; // defaults to simulation result with fuzz factor
+  network?: Network; // defaults to active network
+  payload: TransactionPayload | InputGenerateTransactionPayloadData;
+  secondarySigners?: AccountInput[]; // defaults to no secondary signers
+  sender?: AccountInput; // defaulting to active account (if applicable)
+  sequenceNumber?: number | bigint; // defaulting to sender's sequence number
+  signerAddress?: AccountAddress;
+}
+
+export interface AptosSignTransactionOutputV1_1 {
+  authenticator: AccountAuthenticator;
+  rawTransaction: AnyRawTransaction;
+}
+
+export type AptosSignTransactionMethodV1_1 = (
+  input: AptosSignTransactionInputV1_1
+) => Promise<UserResponse<AptosSignTransactionOutputV1_1>>;
